ed60fd8a05eccf80539825c854836aca19949914
Krognol <=>
2019-07-07 02:56:46 +0200

    initial work on weekly suggestions and db calls

--- main.go
+++ main.go
@@ -7,6 +7,7 @@ import (
 	"log"
 	"os"
 	"os/signal"
+	"strings"
 	"syscall"
 
 	"github.com/Krognol/thronebot/internal"
@@ -58,7 +59,6 @@ func main() {
 
 	// Commands
 	bot.Route.On("pingdb", pingdbHandler(bot.DB)).Use(internal.ElevatedUser).Desc("Pings the database for a connection.")
-
 	var BotID = ses.State.User.ID
 
 	bot.Ses.AddHandler(func(s *discordgo.Session, m *discordgo.MessageCreate) {
@@ -74,8 +74,164 @@ func pingdbHandler(db *sql.DB) router.HandlerFunc {
 	return func(ctx *router.Context) {
 		res := db.Ping()
 		if res == driver.ErrBadConn {
-			log.Print("bot: bad DB conn:", res)
+			log.Println("bot: bad DB conn:", res)
 		}
 		ctx.Reply(res.Error())
+	}
+}
+
+func weeklySuggestionHandler(db *sql.DB) router.HandlerFunc {
+	return func(ctx *router.Context) {
+		suggestCount := getUserSuggestionCount(db, ctx.Msg.Author.ID)
+		if suggestCount >= 3 {
+			ctx.Reply("You've already made 3 suggestions this week.")
+			return
+		}
+
+		build := strings.Split(strings.ToLower(ctx.Args.Get(0)), "/")
+		if len(build) < 4 {
+			ctx.Reply(ctx.Msg.Author.Mention(), "Too few arguments")
+			return
+		}
+
+		var (
+			char  = build[0]
+			skin  = build[1] == "1"
+			weap  = build[2]
+			crown = build[3]
+		)
+
+		if _, ok := internal.Chars[char]; !ok {
+			ctx.Reply("Invalid character")
+			return
+		}
+
+		if _, ok := internal.Weapons[weap]; !ok {
+			ctx.Reply("Invalid weapon")
+			return
+		}
+
+		if _, ok := internal.Crowns[crown]; !ok {
+			ctx.Reply("Invalid crown")
+			return
+		}
+
+		somethingBanned, err := isBanned(db, build[0], build[2], build[3])
+		if err != nil {
+			ctx.Reply("Error while checking for banned items.")
+			return
+		}
+
+		if somethingBanned {
+			ctx.Reply("One or more of your selections are currently banned. Remember to check the banned list for banned items every week.")
+			return
+		}
+
+		err = insertSuggestion(db, ctx.Msg.Author.ID, char, weap, crown, skin)
+		if err != nil {
+			ctx.Reply("Failed to save suggestion.")
+			return
+		}
+
+		incrementUserSuggestionCount(db, ctx.Msg.Author.ID)
+	}
+}
+
+func isBanned(db *sql.DB, char, weap, crown string) (bool, error) {
+	rows, err := db.Query(
+		"SELECT * FROM weekly_banned WHERE chars = ? OR weaps = ? OR crowns = ?;",
+		char,
+		weap,
+		crown,
+	)
+
+	if err != nil {
+		log.Println("checkBanned: failed to retrieve rows:", err)
+		return false, err
+	}
+
+	// Should maybe cache result
+	var bannedChar, bannedCrown, bannedWeap string
+	for rows.Next() {
+		rows.Scan(&bannedChar, &bannedCrown, &bannedWeap)
+		if bannedChar != "" || bannedCrown != "" || bannedWeap != "" {
+			return true, nil
+		}
+	}
+
+	return false, nil
+}
+
+func getUserSuggestionCount(db *sql.DB, id string) int {
+	rows, err := db.Query("SELECT count FROM user_suggestions WHERE id = ?;", id)
+	if err != nil {
+		log.Println("getUserSuggestionCount: failed to get rows:", err)
+		return -1
+	}
+
+	var count int
+	for rows.Next() {
+		rows.Scan(&count)
+	}
+
+	return count
+}
+
+func insertSuggestion(db *sql.DB, uid, char, weap, crown string, skin bool) error {
+	// order is uid, char, skin, weap, crown
+	stmt, err := db.Prepare("INSERT INTO weekly_suggestions VALUES(?, ?, ?, ?);")
+	if err != nil {
+		log.Println("inserSuggestion: failed to prepare stmt:", err)
+		return err
+	}
+
+	useSkin := 0
+	if skin {
+		useSkin = 1
+	}
+
+	_, err = stmt.Exec(uid, char, useSkin, weap, crown)
+	if err != nil {
+		log.Println("inserSuggestion: failed to insert into table stmt:", err)
+		return err
+	}
+	return nil
+}
+
+func incrementUserSuggestionCount(db *sql.DB, uid string) {
+	rows, err := db.Query("SELECT COUNT(*) FROM user_suggestions WHERE user = ?;", uid)
+	if err != nil {
+		log.Println("incrementUserSuggestionCount: failed to retrieve row count:", err)
+		return
+	}
+
+	var count int
+	for rows.Next() {
+		rows.Scan(&count)
+	}
+
+	if count == 0 {
+		stmt, err := db.Prepare("INSERT INTO user_suggestions VALUES(?, ?);")
+		if err != nil {
+			log.Panicln("incrementUserSuggestionCount: failed to prepare stmt:", err)
+			return
+		}
+
+		_, err = stmt.Exec(uid, 1)
+		if err != nil {
+			log.Println("incrementUserSuggestionCount: failed to execute insert:", err)
+			return
+		}
+	} else {
+		stmt, err := db.Prepare("UPDATE user_suggestions SET count = count + 1 WHERE user = ?;")
+		if err != nil {
+			log.Println("incrementUserSuggestionCount: failed to prepare update stmt:", err)
+			return
+		}
+
+		_, err = stmt.Exec(uid)
+		if err != nil {
+			log.Println("incrementUserSuggestionCount: failed to update user count:", err)
+		}
 	}
 }
