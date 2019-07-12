4a205d6ee718db9139b78287dbac642a8da5a763
Krognol <=>
2019-07-12 18:15:11 +0200

    config+handler, item ban, weekly enable/disable

--- main.go
+++ main.go
@@ -3,6 +3,7 @@ package main
 import (
 	"database/sql"
 	"database/sql/driver"
+	"encoding/json"
 	"flag"
 	"fmt"
 	"log"
@@ -19,20 +20,41 @@ import (
 	_ "github.com/mattn/go-sqlite3"
 )
 
+type config struct {
+	ArchiveRepo      string `json:"archive_repo"`
+	DatabasePath     string `json:"database_path"`
+	LogPath          string `json:"log_path"`
+	WeeklySuggestion string `json:"weekly_suggestion"`
+	WeeklyVoting     string `json:"weekly_voting"`
+	Staff            string `json:"staff"`
+}
+
 var (
-	tbAPIKey          = flag.String("tb", "", "Thronebot API key")
-	discordBotKey     = flag.String("t", "", "Discord bot key")
-	githubAPIKey      = flag.String("git", "", "Github API key. For archiving of pins")
-	githubArchiveRepo = flag.String("rep", "", "Repo name")
-	dbName            = flag.String("db", "db.sqlite", "Path to SQLite DB file")
-	logFile           = flag.String("log", "", "Path to file to print logging to")
+	tbAPIKey      = flag.String("tb", "", "Thronebot API key")
+	discordBotKey = flag.String("t", "", "Discord bot key")
+	githubAPIKey  = flag.String("git", "", "Github API key. For archiving of pins")
+	configPath    = flag.String("cfg", "config.json", "Path to config file.")
 )
 
 func main() {
-	if *logFile == "" {
+	cfg := new(config)
+	func() {
+		f, err := os.Open(*configPath)
+		if err != nil {
+			panic(err)
+		}
+		defer f.Close()
+
+		err = json.NewDecoder(f).Decode(cfg)
+		if err != nil {
+			panic(err)
+		}
+	}()
+
+	if cfg.LogPath == "" {
 		log.SetOutput(os.Stdout)
 	} else {
-		f, err := os.Open(*logFile)
+		f, err := os.Open(cfg.LogPath)
 		if err != nil {
 			// Can't open/create file, something's wrong
 			panic(err)
@@ -49,7 +71,7 @@ func main() {
 		log.Fatal("Missing Thronebutt API key.")
 	}
 
-	db, err := sql.Open("sqlite3", *dbName)
+	db, err := sql.Open("sqlite3", cfg.DatabasePath)
 	if err != nil {
 		log.Fatal(err)
 	}
@@ -67,15 +89,22 @@ func main() {
 
 	tbClient := tbapi.New(*tbAPIKey)
 	// Commands
+	bot.Route.On("config", func(ctx *router.Context) {
+		ctx.Reply(
+			"Current config settings\n  Staff:", cfg.Staff,
+			"\n  Weekly voting:", cfg.WeeklyVoting,
+			"\n  Weekly suggestion:", cfg.WeeklySuggestion,
+		)
+	}).On("set", cfgSetHandler(cfg))
+
 	bot.Route.On("pingdb", pingdbHandler(bot.DB)).Use(internal.ElevatedUser).Desc("Pings the database for a connection.")
 
 	bot.Route.On("weekly", nil).
 		On("suggest", weeklySuggestionHandler(bot.DB)).
 		Desc("Suggest a weekly. Ex. `steroids/b/grenade launcher/crown of death`").
-		// TODO print banned things
-		On("banned", nil).Desc("Print banned selections.").
+		On("banned", internal.GetBannedHandler(bot.DB)).Desc("Print banned selections.").
 		// TODO
-		On("ban", nil).Use(internal.ElevatedUser).
+		On("ban", weeklyBanUnbanHandler(bot.DB)).Use(internal.ElevatedUser).
 		On("enable", weeklyEnableDisableHandler(tbClient, true)).Use(internal.ElevatedUser).
 		On("disable", weeklyEnableDisableHandler(tbClient, false)).Use(internal.ElevatedUser).
 		On("set", nil).Use(internal.ElevatedUser)
@@ -84,10 +113,10 @@ func main() {
 		// TODO register archiving routes
 	}
 
-	var BotID = ses.State.User.ID
+	var botID = ses.State.User.ID
 
 	bot.Ses.AddHandler(func(s *discordgo.Session, m *discordgo.MessageCreate) {
-		bot.Route.FindAndExecute(s, "thronebot", BotID, m.Message)
+		bot.Route.FindAndExecute(s, "thronebot", botID, m.Message)
 	})
 
 	closer := make(chan os.Signal, 1)
@@ -95,6 +124,92 @@ func main() {
 
 	fmt.Println("Bot is running. Ctrl+C to quit.")
 	<-closer
+
+	f, err := os.Open(*configPath)
+	if err != nil {
+		log.Println("failed to open config file:", err)
+		os.Exit(1)
+	}
+
+	err = json.NewEncoder(f).Encode(cfg)
+	if err != nil {
+		log.Println("failed to save config to file:", err)
+		os.Exit(1)
+	}
+}
+
+func weeklyBanUnbanHandler(db *sql.DB) router.HandlerFunc {
+	return func(ctx *router.Context) {
+		if len(ctx.Args) < 3 {
+			ctx.Reply(
+				"Usage: `thronebot weekly ban [add|del] [crown|char|wep] (name)`\n",
+				"To ban an item: ex: `thronebot weekly ban add crown crown of blood\n",
+				"To unban an item: ex: `thronebot weekly ban del char steroids",
+			)
+			return
+		}
+
+		addel := ctx.Args.Get(0)
+		kind := ctx.Args.Get(1)
+		which := ctx.Args.After(2)
+		var err error
+		var val int
+
+		switch kind {
+		case "char":
+			val = internal.Chars.NameToID(which)
+		case "crown":
+			val = internal.Crowns.NameToID(which)
+		case "wep":
+			val = internal.Weapons.NameToID(which)
+		default:
+			ctx.Reply("Invalid option: ", which)
+			return
+		}
+
+		if val == -1 {
+			ctx.Reply("Invalid selection: ", which)
+			return
+		}
+
+		switch addel {
+		case "add":
+			err = internal.WeeklyBanAdd(db, kind, val)
+		case "del":
+			err = internal.WeeklyBanDel(db, kind, val)
+		default:
+			ctx.Reply("Invalid option: ", addel, "\n Expected `add` or `del`")
+			return
+		}
+
+		if err != nil {
+			ctx.Reply("Failed to ban item: ", err)
+		}
+	}
+}
+
+func cfgSetHandler(cfg *config) router.HandlerFunc {
+	return func(ctx *router.Context) {
+		prop, val := ctx.Args.Get(0), ctx.Args.Get(1)
+		if prop == "" || val == "" {
+			ctx.Reply("Missing property name or value")
+			return
+		}
+
+		switch prop {
+		case "weekly_suggestion":
+			cfg.WeeklySuggestion = val
+		case "weekly_voting":
+			cfg.WeeklyVoting = val
+		case "staff":
+			cfg.Staff = val
+		default:
+			ctx.Reply("Invalid property name")
+			return
+		}
+		log.Println("cfgSet: set property '", prop, "' to value '", val, "'")
+		ctx.Reply("Set ", prop, " to, ", val)
+	}
 }
 
 func weeklyEnableDisableHandler(tbc *tbapi.Client, enable bool) router.HandlerFunc {
@@ -119,44 +234,6 @@ func weeklyEnableDisableHandler(tbc *tbapi.Client, enable bool) router.HandlerFu
 	}
 }
 
-func getBannedHandler(db *sql.DB) router.HandlerFunc {
-	return func(ctx *router.Context) {
-		rows, err := db.Query("SELECT * FROM weekly_banned;")
-		if err != nil {
-			log.Println("getBanned: failed to query db:", err)
-			ctx.Reply("Failed to retrieve banned items.")
-			return
-		}
-
-		defer rows.Close()
-
-		var (
-			char, crown, wep    string
-			chars, crowns, weps []string
-		)
-
-		// TODO find more efficient solution
-		for rows.Next() {
-			err = rows.Scan(&char, &crown, &wep)
-			if err != nil {
-				log.Fatal("getBanned: failed to scan row:", err)
-				ctx.Reply("Failed to retrieve banned items.")
-				return
-			}
-
-			chars = append(chars, char)
-			crowns = append(crowns, crown)
-			weps = append(weps, wep)
-		}
-
-		ctx.Reply(
-			"Currently banned items:\n\n**Characters:**\n  ", strings.Join(chars, ", "),
-			"\n**Crowns:**\n  ", strings.Join(crowns, ", "),
-			"**Weapons:**\n  ", strings.Join(weps, ", "),
-		)
-	}
-}
-
 func pingdbHandler(db *sql.DB) router.HandlerFunc {
 	return func(ctx *router.Context) {
 		res := db.Ping()
@@ -169,7 +246,7 @@ func pingdbHandler(db *sql.DB) router.HandlerFunc {
 
 func weeklySuggestionHandler(db *sql.DB) router.HandlerFunc {
 	return func(ctx *router.Context) {
-		suggestCount := getUserSuggestionCount(db, ctx.Msg.Author.ID)
+		suggestCount := internal.GetUserSuggestionCount(db, ctx.Msg.Author.ID)
 		if suggestCount >= 3 {
 			ctx.Reply("You've already made 3 suggestions this week.")
 			return
@@ -203,7 +280,7 @@ func weeklySuggestionHandler(db *sql.DB) router.HandlerFunc {
 			return
 		}
 
-		somethingBanned, err := isBanned(db, build[0], build[2], build[3])
+		somethingBanned, err := internal.IsBanned(db, build[0], build[2], build[3])
 		if err != nil {
 			ctx.Reply("Error while checking for banned items.")
 			return
@@ -214,105 +291,11 @@ func weeklySuggestionHandler(db *sql.DB) router.HandlerFunc {
 			return
 		}
 
-		err = insertSuggestion(db, ctx.Msg.Author.ID, char, weap, crown, skin)
+		err = internal.InsertSuggestion(db, ctx.Msg.Author.ID, char, weap, crown, skin)
 		if err != nil {
 			ctx.Reply("Failed to save suggestion.")
+		} else {
+			// TODO post to weekly voting channel
 		}
 	}
-}
-
-func isBanned(db *sql.DB, char, weap, crown string) (bool, error) {
-	rows, err := db.Query(
-		"SELECT * FROM weekly_banned WHERE chars = ? OR weaps = ? OR crowns = ?;",
-		char,
-		weap,
-		crown,
-	)
-
-	if err != nil {
-		log.Println("checkBanned: failed to retrieve rows:", err)
-		return false, err
-	}
-
-	defer rows.Close()
-
-	// Should maybe cache result
-	var bannedChar, bannedCrown, bannedWeap string
-	for rows.Next() {
-		rows.Scan(&bannedChar, &bannedCrown, &bannedWeap)
-		if bannedChar != "" || bannedCrown != "" || bannedWeap != "" {
-			return true, nil
-		}
-	}
-
-	return false, nil
-}
-
-func getUserSuggestionCount(db *sql.DB, id string) int {
-	rows, err := db.Query("SELECT count FROM user_suggestions WHERE id = ?;", id)
-	if err != nil {
-		log.Println("getUserSuggestionCount: failed to get rows:", err)
-		return -1
-	}
-
-	defer rows.Close()
-
-	var count int
-	for rows.Next() {
-		rows.Scan(&count)
-	}
-
-	return count
-}
-
-func insertSuggestion(db *sql.DB, uid, char, weap, crown string, skin bool) error {
-	// order is uid, char, skin, weap, crown
-	stmt, err := db.Prepare("INSERT INTO weekly_suggestions VALUES(?, ?, ?, ?);")
-	if err != nil {
-		log.Println("inserSuggestion: failed to prepare stmt:", err)
-		return err
-	}
-
-	defer stmt.Close()
-
-	useSkin := 0
-	if skin {
-		useSkin = 1
-	}
-
-	_, err = stmt.Exec(uid, char, useSkin, weap, crown)
-	if err != nil {
-		log.Println("inserSuggestion: failed to insert into table stmt:", err)
-		return err
-	}
-
-	return updateSuggestionCount(db, uid)
-}
-
-func updateSuggestionCount(db *sql.DB, uid string) (err error) {
-	var (
-		tx   *sql.Tx
-		stmt *sql.Stmt
-	)
-
-	tx, err = db.Begin()
-	if err != nil {
-		log.Println("updateSuggestionCount: failed to begin Tx:", err)
-		return
-	}
-
-	stmt, err = tx.Prepare("INSERT INTO user_suggestions(id, count) VALUES(?, ?) ON CONFLICT(id) DO UPDATE SET count = count + 1;")
-	if err != nil {
-		log.Println("updateSuggestionCount: failed to prepare stmt:", err)
-		return tx.Rollback()
-	}
-
-	defer stmt.Close()
-
-	_, err = stmt.Exec(uid, 1)
-	if err != nil {
-		log.Println("updateSuggestionCount: failed to exec stmt:", err)
-		return tx.Rollback()
-	}
-	return tx.Commit()
 }
